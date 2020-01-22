d4f93dd28de868ff8722c466a7c71243192acdbe
Jesús Christian Castillo <christian.castillo.lozano@gmail.com>
2020-01-21 21:46:34 -0600

    Simple example

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-$conn = mysqli_connect(
+/*$conn = mysqli_connect(
     'localhost',
     'khristian',
     '12345678',
@@ -8,9 +8,25 @@ $conn = mysqli_connect(
 );
 
 //Prueba para revisar conneccion a db
-/*if(isset($conn)){
+if(isset($conn)){
     echo 'DB is connected';
 }*/
 
+$url = getenv('JAWSDB_MARIA_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connection was successfully established!";
 
 ?>
